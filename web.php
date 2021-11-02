78dfc0c68194ac017dcaacfb09953c2ba0e3c649
Tazkia028 <90827130+Tazkia028@users.noreply.github.com>
2021-11-02 20:00:11 +0700

    Add files via upload

--- web.php
+++ web.php
@@ -22,29 +22,6 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/tampil_mahasiswa', function () {
-    return view('datamahasiswa');
-});
-
-Route::get('/menumasakan', function () {
-    return view('menumasakan');
-});
-
-
-
-Route::get('/daftar_mahasiswa', [MahasiswaController::class, 'index']);
-// routing form dan list buku
-Route::get('/form-buku', [FormbukuController::class, 'index']);
-Route::get('/list-buku', [ListController::class, 'index']);
-
-//routing data barang
-route::get('/databarang', function() {
-    return view('databarang');
-});
-    
-route::get('/databarang',
-    [BarangController::class, 'data_barang']);
-
 //source code 2
 Route::get('/formulir', [GuruController::class, 'formulir']);
 Route::post('/formulir/proses', [GuruController::class, 'formulir_proses']);
